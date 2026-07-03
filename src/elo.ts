34d31e99b38a613b71363719692559199ee0fd94
Paul Irish <commits@paul.irish>
2026-07-03 10:57:28 -0700

    Update default min-games leaderboard threshold to 10

--- src/elo.ts
+++ src/elo.ts
@@ -9,7 +9,7 @@ export interface EloConfig {
 const DEFAULT_CONFIG: EloConfig = {
   defaultRating: 1000,
   kFactor: 32,
-  minGamesForLeaderboard: 5
+  minGamesForLeaderboard: 10
 };
 
 export class EloCalculator {
